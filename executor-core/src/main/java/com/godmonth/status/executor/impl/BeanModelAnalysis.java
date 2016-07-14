cb4f34f9f90e0a12a3957997a8d48989b6340744
godmonth <link2ibm@126.com>
2016-07-14 15:01:11 +0800

    c

--- executor-core/src/main/java/com/godmonth/status/executor/impl/BeanModelAnalysis.java
+++ executor-core/src/main/java/com/godmonth/status/executor/impl/BeanModelAnalysis.java
@@ -8,6 +8,9 @@ import com.godmonth.status.executor.intf.ModelAnalysis;
 import jodd.bean.BeanUtil;
 
 public class BeanModelAnalysis<MODEL, VALUE, STATUS> implements ModelAnalysis<MODEL, STATUS> {
+
+	private Class<MODEL> modelClass;
+
 	private VALUE expectedValue;
 
 	private String typePropertyName;
@@ -16,6 +19,9 @@ public class BeanModelAnalysis<MODEL, VALUE, STATUS> implements ModelAnalysis<MO
 
 	@Override
 	public void validate(MODEL model) {
+		if (modelClass != null) {
+			Validate.isTrue(modelClass.equals(model.getClass()));
+		}
 		if (StringUtils.isNotBlank(typePropertyName) && expectedValue != null) {
 			VALUE actualValue = BeanUtil.silent.getProperty(model, typePropertyName);
 			Validate.isTrue(expectedValue.equals(actualValue), "expected:%s,actual:%s", expectedValue, actualValue);
@@ -37,6 +43,10 @@ public class BeanModelAnalysis<MODEL, VALUE, STATUS> implements ModelAnalysis<MO
 
 	public void setStatusPropertyName(String statusPropertyName) {
 		this.statusPropertyName = statusPropertyName;
+	}
+
+	public void setModelClass(Class<MODEL> modelClass) {
+		this.modelClass = modelClass;
 	}
 
 }
