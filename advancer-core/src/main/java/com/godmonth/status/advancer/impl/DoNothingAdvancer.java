b7a42c0949b835c8c2cd76b5e914cf4791b80619
godmonth <link2ibm@126.com>
2017-03-10 13:47:28 +0800

    x

--- advancer-core/src/main/java/com/godmonth/status/advancer/impl/DoNothingAdvancer.java
+++ advancer-core/src/main/java/com/godmonth/status/advancer/impl/DoNothingAdvancer.java
@@ -1,6 +1,7 @@
 package com.godmonth.status.advancer.impl;
 
 import com.godmonth.status.advancer.intf.AdvancedResult;
+import com.godmonth.status.advancer.intf.NextOperation;
 import com.godmonth.status.transitor.tx.intf.TriggerBehavior;
 
 /**
@@ -9,19 +10,13 @@ import com.godmonth.status.transitor.tx.intf.TriggerBehavior;
  */
 public class DoNothingAdvancer<MODEL, INST, TRIGGER> extends InstructionAdvancer<MODEL, INST, TRIGGER> {
 
-	private TRIGGER trigger;
+	protected TRIGGER trigger;
+
+	protected NextOperation nextOperation = NextOperation.ADVANCE;
 
 	@Override
 	protected AdvancedResult<MODEL, TRIGGER> doAdvance(MODEL model, Object message) {
-		return new AdvancedResult<>(new TriggerBehavior<TRIGGER, MODEL>(trigger));
-	}
-
-	public void setTrigger(TRIGGER trigger) {
-		this.trigger = trigger;
-	}
-
-	public TRIGGER getTrigger() {
-		return trigger;
+		return new AdvancedResult<>(new TriggerBehavior<TRIGGER, MODEL>(trigger), nextOperation);
 	}
 
 }
