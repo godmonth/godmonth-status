853b2d89c66507de3921120ad8f973710f5b99e0
godmonth <link2people@gmail.com>
2020-12-17 16:58:54 +0400

    update

--- transitor-def/src/main/java/com/godmonth/status/transitor/definition/intf/TriggerDefinition.java
+++ transitor-def/src/main/java/com/godmonth/status/transitor/definition/intf/TriggerDefinition.java
@@ -1,43 +1,17 @@
 package com.godmonth.status.transitor.definition.intf;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import lombok.Data;
 
 /**
- * 
- * @author shenyue
- *
  * @param <TRIGGER>
  * @param <STATUS>
+ * @author shenyue
  */
+@Data
 public class TriggerDefinition<TRIGGER, STATUS> {
 
-	private TRIGGER trigger;
+    private TRIGGER trigger;
 
-	private STATUS nextStatus;
-
-	public TRIGGER getTrigger() {
-		return trigger;
-	}
-
-	public void setTrigger(TRIGGER trigger) {
-		this.trigger = trigger;
-	}
-
-	public STATUS getNextStatus() {
-		return nextStatus;
-	}
-
-	public void setNextStatus(STATUS nextStatus) {
-		this.nextStatus = nextStatus;
-	}
-
-	/**
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
-		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE).append("nextStatus", this.nextStatus)
-				.append("trigger", this.trigger).toString();
-	}
+    private STATUS nextStatus;
 
 }
