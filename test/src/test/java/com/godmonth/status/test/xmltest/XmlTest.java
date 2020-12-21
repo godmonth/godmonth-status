27f63ca87888721eca8b89da71118c6bd372216c
godmonth <link2people@gmail.com>
2020-12-21 15:01:12 +0400

    update

--- test/src/test/java/com/godmonth/status/test/xmltest/XmlTest.java
+++ test/src/test/java/com/godmonth/status/test/xmltest/XmlTest.java
@@ -1,5 +1,6 @@
 package com.godmonth.status.test.xmltest;
 
+import com.godmonth.status.advancer.intf.SyncResult;
 import com.godmonth.status.executor.intf.OrderExecutor;
 import com.godmonth.status.test.sample.domain.SampleModel;
 import com.godmonth.status.test.sample.domain.SampleStatus;
@@ -34,6 +35,8 @@ public class XmlTest {
         SampleModel sampleModel = new SampleModel();
         sampleModel.setStatus(SampleStatus.CREATED);
         sampleModel.setType("test");
-        System.out.println(sampleModelExecutor);
+        SampleModel sampleModel1 = sampleModelRepository.save(sampleModel);
+        SyncResult<SampleModel, ?> execute = sampleModelExecutor.execute(sampleModel, null, null);
+        System.out.println(execute);
     }
 }
