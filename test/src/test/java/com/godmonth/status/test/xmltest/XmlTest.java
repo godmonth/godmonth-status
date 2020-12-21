4edcc623e614304fe4517534693a79ba7b58f5e0
godmonth <link2people@gmail.com>
2020-12-21 12:29:28 +0400

    update

--- test/src/test/java/com/godmonth/status/test/xmltest/XmlTest.java
+++ test/src/test/java/com/godmonth/status/test/xmltest/XmlTest.java
@@ -1,6 +1,7 @@
 package com.godmonth.status.test.xmltest;
 
 import com.godmonth.status.test.sample.repo.RepoConfig;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,8 +15,10 @@ import org.springframework.context.annotation.ImportResource;
 @AutoConfigureDataJpa
 @SpringBootTest(classes = {RepoConfig.class})
 @ImportResource(locations = {"classpath:/sample-bean.xml"})
-
 @EnableAutoConfiguration
 public class XmlTest {
-
+    @Test
+    void name() {
+        System.out.println("ok");
+    }
 }
