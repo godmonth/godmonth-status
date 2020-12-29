944507991a111c775bf9654bf749136aca630c31
godmonth <link2people@gmail.com>
2020-12-29 17:12:38 +0400

    update 2ds

--- test/src/test/java/com/godmonth/status/test/sample/machine/cfg3/Sample3Test.java
+++ test/src/test/java/com/godmonth/status/test/sample/machine/cfg3/Sample3Test.java
@@ -8,13 +8,19 @@ import com.godmonth.status.test.sample.domain.SampleModel;
 import com.godmonth.status.test.sample.domain.SampleStatus;
 import com.godmonth.status.test.sample.repo.SampleModelRepository;
 import com.godmonth.status.test.sample.repo2.SampleModelReadOnlyRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 /**
@@ -36,19 +42,42 @@ public class Sample3Test {
     @Autowired
     private SampleModelReadOnlyRepository sampleModelReadOnlyRepository;
 
+    @Qualifier("d2")
+    @Autowired
+    private DataSource dataSource2;
+
+    @Qualifier("d3")
+    @Autowired
+    private DataSource dataSource3;
+
     @Test
-    void name() {
+    void name() throws SQLException {
         SampleModel sampleModel = new SampleModel();
         sampleModel.setStatus(SampleStatus.CREATED);
         sampleModel.setType("test");
         SampleModel sampleModel1 = sampleModelRepository.save(sampleModel);
         SyncResult<SampleModel, ?> execute = sampleModelOrderExecutor.execute(sampleModel1, "eee", "fff");
         System.out.println(execute);
+        {
+            Connection connection = dataSource2.getConnection();
+            ResultSet resultSet = connection.createStatement().executeQuery("select count(*) from sample_model");
+            resultSet.next();
+            Assertions.assertEquals(resultSet.getInt(1), 1);
+            String string = resultSet.getString(1);
+            System.out.println(string);
+        }
+        {
+            //查看数据源3
+            Connection connection3 = dataSource3.getConnection();
+            ResultSet resultSet3 = connection3.createStatement().executeQuery("select count(*) from sample_model");
+            resultSet3.next();
+            Assertions.assertEquals(resultSet3.getInt(1), 0);
+        }
+        //查看数据源2
         Optional<SampleModel> byId = sampleModelRepository.findById(1L);
-        System.out.println(byId.isPresent());
-        System.out.println(byId.get());
+        Assertions.assertTrue(byId.isPresent());
         Optional<SampleModel> byId2 = sampleModelReadOnlyRepository.findById(1L);
-        System.out.println(byId2.isPresent());
-        System.out.println(byId2.get());
+        Assertions.assertFalse(byId2.isPresent());
+
     }
 }
