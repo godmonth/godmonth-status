82b20c9de47d9da94a2bde22cf1496314ba7b28c
godmonth <link2people@gmail.com>
2021-01-03 09:45:48 +0400

    update

--- builder/src/test/java/com/godmonth/status/builder/transitor/JsonDefinitionBuilderTest.java
+++ builder/src/test/java/com/godmonth/status/builder/transitor/JsonDefinitionBuilderTest.java
@@ -1,10 +1,5 @@
 package com.godmonth.status.builder.transitor;
 
-import com.godmonth.status.analysis.impl.model.AnnotationBeanModelAnalysis;
-import com.godmonth.status.analysis.impl.model.SimpleBeanModelAnalysis;
-import com.godmonth.status.analysis.impl.sm.AnnotationStateMachineAnalysis;
-import com.godmonth.status.analysis.impl.sm.SimpleStateMachineAnalysis;
-import com.godmonth.status.builder.domain.SampleModel;
 import com.godmonth.status.builder.domain.SampleStatus;
 import com.godmonth.status.builder.domain.SampleTrigger;
 import org.junit.jupiter.api.Assertions;
@@ -22,10 +17,8 @@ import java.util.function.Function;
 class JsonDefinitionBuilderTest {
     @Test
     void jsonString() throws IOException {
-        AnnotationBeanModelAnalysis<SampleModel> modelAnalysis = AnnotationBeanModelAnalysis.<SampleModel>annoBuilder().modelClass(SampleModel.class).build();
-        AnnotationStateMachineAnalysis machineAnalysis = AnnotationStateMachineAnalysis.annoBuilder().modelAnalysis(modelAnalysis).build();
 //        String jsonString = FileUtils.readFileToString(new File("src/test/resources/state-machine-sample.json"), StandardCharsets.UTF_8);
-        Function<SampleStatus, Function<SampleTrigger, SampleStatus>> function = JsonDefinitionBuilder.<SampleStatus, SampleTrigger>builder().stateMachineAnalysis(machineAnalysis).resource(new FileSystemResource("src/test/resources/state-machine-sample.json")).build();
+        Function<SampleStatus, Function<SampleTrigger, SampleStatus>> function = JsonDefinitionBuilder.<SampleStatus, SampleTrigger>builder().statusClass(SampleStatus.class).triggerClass(SampleTrigger.class).resource(new FileSystemResource("src/test/resources/state-machine-sample.json")).build();
         System.out.println(function);
         Assertions.assertTrue(function.apply(SampleStatus.CREATED) != null);
         Assertions.assertTrue(function.apply(SampleStatus.CREATED).apply(SampleTrigger.PAY) != null);
@@ -34,10 +27,7 @@ class JsonDefinitionBuilderTest {
 
     @Test
     void genericType() throws IOException {
-        AnnotationBeanModelAnalysis<SampleModel> modelAnalysis = AnnotationBeanModelAnalysis.<SampleModel>annoBuilder().modelClass(SampleModel.class).build();
-        AnnotationStateMachineAnalysis machineAnalysis = AnnotationStateMachineAnalysis.annoBuilder().modelAnalysis(modelAnalysis).build();
-
-        Function<SampleStatus, Function<SampleTrigger, SampleStatus>> function = JsonDefinitionBuilder.<SampleStatus, SampleTrigger>builder().resource(new FileSystemResource("src/test/resources/state-machine-sample.json")).stateMachineAnalysis(machineAnalysis).build();
+        Function<SampleStatus, Function<SampleTrigger, SampleStatus>> function = JsonDefinitionBuilder.<SampleStatus, SampleTrigger>builder().resource(new FileSystemResource("src/test/resources/state-machine-sample.json")).statusClass(SampleStatus.class).triggerClass(SampleTrigger.class).build();
         System.out.println(function);
         Assertions.assertTrue(function.apply(SampleStatus.CREATED) != null);
         Assertions.assertTrue(function.apply(SampleStatus.CREATED).apply(SampleTrigger.PAY) != null);
@@ -46,9 +36,7 @@ class JsonDefinitionBuilderTest {
 
     @Test
     void stringType() throws IOException {
-        SimpleBeanModelAnalysis<SampleModel2> modelAnalysis = SimpleBeanModelAnalysis.<SampleModel2>builder().modelClass(SampleModel2.class).statusPropertyName("status").build();
-        SimpleStateMachineAnalysis machineAnalysis = SimpleStateMachineAnalysis.builder().modelAnalysis(modelAnalysis).triggerClass(String.class).build();
-        Function<String, Function<String, String>> function = JsonDefinitionBuilder.<String, String>builder().resource(new FileSystemResource("src/test/resources/state-machine-sample.json")).stateMachineAnalysis(machineAnalysis).build();
+        Function<String, Function<String, String>> function = JsonDefinitionBuilder.<String, String>builder().resource(new FileSystemResource("src/test/resources/state-machine-sample.json")).statusClass(String.class).triggerClass(String.class).build();
         System.out.println(function);
         Assertions.assertTrue(function.apply("CREATED") != null);
         Assertions.assertTrue(function.apply("CREATED").apply("PAY") != null);
